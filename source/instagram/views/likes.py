61496f094becd1fd94538df200a19d8999097341
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-21 12:41:52 +0600

    Исправил добавление/удаление лайков согласно замечания преподавателя

--- source/instagram/views/likes.py
+++ source/instagram/views/likes.py
@@ -1,25 +1,20 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
+from django.views import View
 
 from instagram.models import Like, Publication
 
 
-class LikeCreateView(LoginRequiredMixin, CreateView):
-    model = Like
-    fields = []
-
-    def form_valid(self, form):
-        publication_id = self.kwargs['pk']
+class LikeCreateView(LoginRequiredMixin, View):
+    def post(self, request, *args, **kwargs):
+        publication_id = kwargs['pk']
         publication = Publication.objects.get(id=publication_id)
-        if not Like.objects.filter(publication=publication, user=self.request.user).exists():
-            like = form.save(commit=False)
-            like.user = self.request.user
-            like.publication = publication
-            like.save()
+        try:
+            like = Like.objects.get(publication=publication, user=request.user)
+            like.delete()
+            publication.likes_count -= 1
+        except Like.DoesNotExist:
+            like = Like.objects.create(publication=publication, user=request.user)
             publication.likes_count += 1
-            publication.save()
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('publication_detail', args=(self.kwargs['pk'],))
+        publication.save()
+        return redirect(request.META.get('HTTP_REFERER'))
