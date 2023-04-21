1e8bb9521a530091a940e3590fb4bbc46cb89526
Abylay Kuvatov <abukuvatov@icloud.com>
2023-04-21 10:34:52 +0600

    Изменил отображение лайков, то есть добавил сериализатор в Публикацию

--- source/api/serializers.py
+++ source/api/serializers.py
@@ -3,13 +3,15 @@ from rest_framework import serializers
 from instagram.models import Publication, Like
 
 
-class PublicationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Publication
-        exclude = ('is_deleted', 'deleted_at')
-
-
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Like
         fields = ('user', 'publication', 'created_at')
+
+
+class PublicationSerializer(serializers.ModelSerializer):
+    likes = LikeSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Publication
+        exclude = ('is_deleted', 'deleted_at')
--- source/api/urls.py
+++ source/api/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from api.views import PublicationViewSet, LikeViewSet
+from api.views import PublicationViewSet
 
 router = routers.DefaultRouter()
 router.register(r'publications', PublicationViewSet)
-router.register(r'likes', LikeViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
